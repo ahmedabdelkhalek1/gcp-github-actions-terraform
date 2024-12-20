7a8396a42061c6481a30f8cd140280204d9fe4e1
ahmedabdelkhalek1 <medoooo163@gmail.com>
2024-12-20 18:14:20 +0200

    try with vars final1

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,12 +1,17 @@
 project_id = "friendly-brand-441407-b4"
 region     = "us-east1"
 zone       = "us-east1-b"
+subnet_ip_range = "10.0.1.0/24"
+vpc_auto_create_subnets = false
+vpc_mtu = 1460
+
 vm_config = {
     vm_name = "flask-app-1"
     vm_type = "f1-micro"
+    boot_disk_size = 10
     vm_image = "debian-cloud/debian-11"
     vm_startup_script = "sudo apt-get install -yq build-essential python3-pip rsync;pip install flask"
     vm_tags = ["ssh",
             "http-server",
             "flask"]
-}
+            }
--- variables.tf
+++ variables.tf
@@ -11,6 +11,19 @@ variable "zone" {
   
 }
 
+variable "subnet_ip_range" {
+    type = string
+    description = "The IPv4 CIDR range to be used in the VPC that will be created"
+}
+variable "vpc_auto_create_subnets" {
+    type = bool
+    description = "Defines the behavior for the VPC whether we want to use it in auto mode (true) or a custom VPC (false)"
+}
+
+variable "vpc_mtu" {
+    type = number
+    description = "The value of the MTU for our new VPC"
+}
 variable "vm_config" {
   description = "the VM configuration variables "
   type = object({
@@ -18,8 +31,11 @@ variable "vm_config" {
     vm_tags = list(string)
     vm_type = string
     vm_image = string
+    boot_disk_size = number
     vm_startup_script = string
   }
   )
 }
+
+
  
